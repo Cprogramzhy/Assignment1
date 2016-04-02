d78ecdf990b27c0237ec23027204a87c3c9a2a5e
FanJiaoJiao <1049311978@qq.com>
2016-04-02 21:20:17 +0800

    highScore4

---
+++ highScore4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,i,a[100];
+	int max=a[0];
+	cin>>n;
+	cin>>a[i];
+	for(i=0;i<n;i++)
+	{
+	 if  (a[i]>max)
+	 {
+		max=a[i];
+	 }
+	}
+		cout<<max<<endl;
+		return 0;
+}
+
